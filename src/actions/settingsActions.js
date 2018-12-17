b6d001e441084ad9cc67d96b5985f6bcfdd3e91c
Camtasia <farahkhanu@gmail.com>
2018-12-16 16:20:14 -0800

    Created persistence of settings with localStorage

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -2,19 +2,49 @@ import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTRATION} fr
 
 
 export const disableBalanceOnAdd = () =>{
+    // Get settings from localStorage
+    const settings = JSON.parse(localStorage.getItem('settings'));
+
+    // Toggle
+    settings.disableBalanceOnAdd= !settings.disableBalanceOnAdd;
+
+    // Set back to localStorage
+    localStorage.setItem('settings', JSON.stringify(settings));
+
     return {
-        type: DISABLE_BALANCE_ON_ADD
+        type: DISABLE_BALANCE_ON_ADD,
+        payload: settings.disableBalanceOnAdd
     }
 }
 
 export const disableBalanceOnEdit = () =>{
+        // Get settings from localStorage
+        const settings = JSON.parse(localStorage.getItem('settings'));
+
+        // Toggle
+        settings.disableBalanceOnEdit= !settings.disableBalanceOnEdit;
+    
+        // Set back to localStorage
+        localStorage.setItem('settings', JSON.stringify(settings));
+    
     return {
-        type: DISABLE_BALANCE_ON_EDIT
+        type: DISABLE_BALANCE_ON_EDIT,
+        payload: settings.disableBalanceOnEdit
     }
 }
 
 export const allowRegistration = () =>{
+    // Get settings from localStorage
+    const settings = JSON.parse(localStorage.getItem('settings'));
+
+    // Toggle
+    settings.allowRegistration= !settings.allowRegistration;
+
+    // Set back to localStorage
+    localStorage.setItem('settings', JSON.stringify(settings));
+    
     return {
-        type: ALLOW_REGISTRATION
+        type: ALLOW_REGISTRATION,
+        payload: settings.allowRegistration
     }
 }
--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,28 +1,22 @@
 import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTRATION  } from '../actions/types';
 
-const initialState = {
-    disableBalanceOnAdd: true,
-    disableBalanceOnEdit: false,
-    allowRegistration: false
-}
-
-export default function(state= initialState, action) {
+export default function(state= {}, action) {
     switch(action.type) {
         default: return state;
         case DISABLE_BALANCE_ON_ADD:
             return {
                 ...state,
-                disableBalanceOnAdd: !state.disableBalanceOnAdd
+                disableBalanceOnAdd: action.payload
             }
         case DISABLE_BALANCE_ON_EDIT:
             return {
                 ...state, 
-                disableBalanceOnEdit:!state.disableBalanceOnEdit
+                disableBalanceOnEdit: action.payload
             }
         case ALLOW_REGISTRATION:
             return {
                 ...state,
-                allowRegistration: !state.allowRegistration
+                allowRegistration: action.payload
             }
     }
 } 
